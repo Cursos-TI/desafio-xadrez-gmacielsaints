947c63400947c82e7d78c3d411dd501bb89cac23
Gean Maciel <geanplay20@gmail.com>
2025-10-15 14:32:55 +0000

    aventureiro

--- xadrez.c
+++ xadrez.c
@@ -24,5 +24,16 @@ int main() {
         printf("Bispo move para cima\n");
         bispo++;
     } while (bispo <= 5);
+
+//4. Cavalo movendo 2 vezes para baixo e uma pra esquerda (usando For)
+    printf("--- Movimento do Cavalo ---\n");
+    int mov2cavalo, mov1cavalo;
+    for (mov1cavalo = 1; mov1cavalo == 1; mov1cavalo++){
+        mov2cavalo = 1;
+        while (mov2cavalo <= 2){
+        printf("Cavalo move para baixo!\n");
+        mov2cavalo++;}
+    }
+    printf("Cavalo move para a esquerda!\n");
     return 0;
 }
