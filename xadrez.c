627a0a3f21594f9a2c468f050085493db3ab4d4b
Gean Maciel <geanplay20@gmail.com>
2025-11-03 20:41:06 +0000

    nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -1,39 +1,52 @@
 #include <stdio.h>
-
-int main() {
- 
     //1. Rainha movendo 8 vezes para a esquerda (usando For)
-    printf("--- Movimento da Rainha ---\n");
-    for (int i = 1; i < 9; i++) {
-        printf("Rainha move para a esquerda\n");
+    void movRainha(int vezes) {
+        for (int i = 0; i < vezes; i++) {
+            printf("Rainha move para a esquerda\n");
+        }
     }
 
     //2. Torre movendo 5 vezes para a direita (usando While)
-    printf("--- Movimento da Torre ---\n");
-    int torre = 1;
-    while (torre < 6) {
-        torre++;
-        printf("Torre move para a direita\n");
+    void movTorre(int vezes) {
+        int torre = 0;
+        while (torre < vezes) {
+            printf("Torre move para a direita\n");
+            torre++;
+        }
     }
 
     //3. Bispo movendo 4 vezes para a diagonal (usando Do While)
-    printf("--- Movimento do Bispo ---\n");
-    int bispo = 1;
-    do {
-        printf("Bispo move para a direita\n");
-        printf("Bispo move para cima\n");
-        bispo++;
-    } while (bispo <= 5);
-
-//4. Cavalo movendo 2 vezes para baixo e uma pra esquerda (usando For)
-    printf("--- Movimento do Cavalo ---\n");
-    int mov2cavalo, mov1cavalo;
-    for (mov1cavalo = 1; mov1cavalo == 1; mov1cavalo++){
-        mov2cavalo = 1;
-        while (mov2cavalo <= 2){
-        printf("Cavalo move para baixo!\n");
-        mov2cavalo++;}
+    void movBispo(int vezes) {
+        int bispo = 0;
+        do {
+            printf("Bispo move para a direita\n");
+            printf("Bispo move para cima\n");
+            bispo++;
+        } while (bispo < vezes);
     }
+
+    //4. Cavalo movendo 2 vezes para baixo e uma pra esquerda (usando For)
+    void movCavalo(int vezes) {
+    while (vezes > 0) {
+        for (int i = 0; i < 2; i++) {
+        printf("Cavalo move para baixo!\n");
+        }
     printf("Cavalo move para a esquerda!\n");
-    return 0;
-}
+    vezes--;
+    }
+    }
+
+
+
+    ///RECURSIVIDADE///
+    int main() {
+        printf("--- Movimento da Rainha ---\n");
+        movRainha(8);
+        printf("--- Movimento da Torre ---\n");
+        movTorre(5);
+        printf("--- Movimento do Bispo ---\n");
+        movBispo(4);
+        printf("--- Movimento do Cavalo ---\n");
+        movCavalo(1);
+        return 0;
+    }
